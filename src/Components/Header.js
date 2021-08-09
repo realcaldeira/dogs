3e1a8380744e544b64265a8468e61eb2f5303e0b
Lucas Caldeira <mrrealcaldeira@gmail.com>
2021-08-09 18:10:30 -0300

    806 - prox ( correção )

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
+import { ReactComponent as Dogs } from '../Assets/dogs.svg';
 
 const Header = () => {
   return (
-    <div className={styles.header}>
+    <header className={styles.header}>
       Header
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+      <nav className={`${styles.nav} container`}>
+        <Link classname={styles.logo} to="/" aria-label="Dogs - Home">
+          Home
+        </Link>
+        <Dogs />
+        <Link classname={styles.login} to="/login">
+          Login / Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 };
 
