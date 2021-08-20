a16a18eae0ef6b2e4db839afd08c411afea8d718
Lucas Caldeira <mrrealcaldeira@gmail.com>
2021-08-20 17:25:01 -0300

    818

---
+++ src/Hooks/useMedia.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+const useMedia = (media) => {
+  const [match, setMatch] = useState(null);
+
+  useEffect(() => {
+    function changeMatch() {
+      const { matches } = window.matchMedia(media);
+      setMatch(matches);
+    }
+    changeMatch();
+    window.addEventListener('resize', changeMatch);
+  }, [media]);
+  return match;
+};
+
+export default useMedia;
